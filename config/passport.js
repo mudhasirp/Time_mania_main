2c97fbb3f44f3c6d3384f7af044e5c363eb9d0cb
Mudhasir P <mudhasirp9@gmail.com>
2025-03-05 17:50:44 +0530

    changed the google signauth

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ const env=require("dotenv").config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/auth/google/callback",
+    callbackURL: "https://timemania.life/auth/google/callback",
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         let user = await User.findOne({ googleId: profile.id });
